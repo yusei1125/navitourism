023ec930c77f9ebe43af02966dcc2df4dedbfc38
yusei1125 <Sk11250925@gmail.com>
2022-01-31 07:37:12 +0000

    ランキング機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,10 @@
 class HomesController < ApplicationController
   def top
+    @tourism = Tourism.order("updated_at").limit(3)
+    @all_ranks = Tourism.find(Favorite.group(:tourism_id).order('count(tourism_id) desc').limit(5).pluck(:tourism_id))
+    @tourism_comment_ranks = Tourism.find(TourismComment.group(:tourism_id).order('count(tourism_id) desc').limit(5).pluck(:tourism_id))
+  end
+
+  def about
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_083931) do
+ActiveRecord::Schema.define(version: 2022_01_31_065447) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_083931) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
